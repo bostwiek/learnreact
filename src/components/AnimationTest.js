20fa25f5f5b06133249c20930591b00f1b6ea7e0
bostwiek <bostwiek@users.noreply.github.com>
2020-05-06 20:25:36 -0600

    testing animations in react

--- src/components/AnimationTest.js
+++ src/components/AnimationTest.js
@@ -1,99 +1,28 @@
-import React from 'react'
-import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group'
+import React, {useState} from 'react'
+import { Transition } from 'react-transition-group'
 import '../Animations.css'
-import uuid from 'uuid'
 
-class AnimationTest extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			id: uuid(),
-			name: 'XD',
-			count: 0,
-			bool: true,
-			sidebar: false
-		}
-		this.countUp = this.countUp.bind(this);
-		this.flipBool = this.flipBool.bind(this);
-	}
+function AnimationTest() {
 
-	countUp() {
-		let currentCount = this.state.count;
-		currentCount += 1;
-		this.setState({ count: currentCount })
-	}
+	const [entered, setEntered] = useState(false);
 
-	flipBool() {
-		let currentBool = this.state.bool;
-		currentBool = !currentBool;
-		this.setState({bool: currentBool})
-	}
+	return (
+		<div>
+			<AComponent in={entered} />
+			<button onClick={()=>{setEntered(!entered)}}>Toggle Entered</button>
+		</div>
+	)
 
-
-	render() {
-
-		const { appearHome } = this.state;
-
-
-		return (
-			<>
-				<div className="animation-container">
-
-
-					<Transition
-						in={this.state.bool}
-						timeout={1000}
-						appear
-					>
-						{(status) => (
-
-							<div className={`test test-${status}`}>
-								react-transition-group status: {status}
-							</div>
-
-						)}
-					</Transition>
-
-					<div className="count-container">
-						{this.state.count}
-					</div>
-
-					<button onClick={this.flipBool}>Boop</button>
-
-				</div>
-
-
-				<CSSTransition
-					in={this.state.sidebar}
-					classNames="sidebar"
-				>
-					<div className="sidebar">
-						<ul>
-							<li><a onClick={()=>{
-								let flipBool = !this.state.bool;
-								this.setState({bool: flipBool})
-							}}>Home</a></li>
-							<li><a>Store</a></li>
-							<li><a>About</a></li>
-							<li><a>Contact</a></li>
-						</ul>
-					</div>
-				</CSSTransition>
-				
-				<div className="button-container">
-					<button onClick={()=>{
-							let sidebarFlip = !this.state.sidebar;
-							this.setState({sidebar: sidebarFlip})
-						}}>=</button>
-				</div>
-
-
-							
-
-
-			</>
-		)
-	}
 }
+
+const AComponent = ({in: inProp}) => (
+	<Transition in={inProp} timeout={500} unmountOnExit>
+		{state => (
+			<div>
+				I am {state}
+			</div>
+		)}
+	</Transition>
+)
 
 export default AnimationTest;
