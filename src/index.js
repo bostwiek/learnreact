611ab3ceb472b39c0444412a7cd14265111994b2
bostwiek <bostwiek@users.noreply.github.com>
2020-08-24 21:21:31 -0600

    add offset delay AND IT WORKS!!!

--- src/index.js
+++ src/index.js
@@ -13,10 +13,34 @@ function Example() {
 
   return (
 		<>
-
+				{/*
+					data-aos="fade-zoom-in"
+					data-aos-easing="ease-in-back"
+					data-aos-delay="300"
+					data-aos-offset="0"
+				*/}
 			<h1>Animate on scroll tests!</h1>
 
-			<div data-aos="fade-left" className="box"></div>
+			<div data-aos="fade-left" className="box">
+				<p
+					data-aos="fade-zoom-left"
+					data-aos-delay="300"
+				>
+					300
+				</p>
+				<p
+					data-aos="fade-zoom-left"
+					data-aos-delay="600"
+				>
+					600
+				</p>
+				<p
+					data-aos="fade-zoom-left"
+					data-aos-delay="1000"
+				>
+					1000
+				</p>
+			</div>
 			<div data-aos="fade-right" className="box"></div>
 			<div data-aos="fade-left" className="box"></div>
 			<div data-aos="fade-right" className="box"></div>
