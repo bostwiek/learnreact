d93b29a31ec5c81256d039acd6bdd41f7f9e2a6d
bostwiek <bostwiek@users.noreply.github.com>
2020-09-10 21:11:55 -0600

    organize notes, playing with useEffect and useState hooks

---
+++ src/aos-notes.js
@@ -0,0 +1,29 @@
+/*
+	animate-on-scroll keywords:
+
+		data-aos="fade-up"
+			fade-up-right
+			fade-zoom-in
+			flip-left
+			zoom-in
+			zoom-in-left
+			zoom-out-left
+			
+		data-aos-easing="ease-in-back"
+			linear
+			ease-in-sine
+			ease-in-back
+			ease-out-cubic
+		
+		data-aos-anchor-placement="center-center"
+			top-center
+			bottom-left
+			top-bottom (???)
+
+		data-aos-duration="1500"
+
+		data-aos-offset="300"
+
+		data-aos-delay="300" 
+		
+	*/
